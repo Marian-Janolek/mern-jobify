fec4eb46834e05f6a453b41f729401821e25ab58
Marian-Janolek <m.janolek@gmail.com>
2022-01-10 17:56:22 +0100

    add error page, register, login, dashboard, routes and basic structure of project

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Landing, Register, Dashboard, Error } from './pages';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,71 @@
+import main from '../assets/images/main.svg';
+import styled from 'styled-components';
+import { Logo } from '../components';
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
+            magnam reiciendis qui vero iste aperiam illum voluptatibus accusamus
+            perferendis delectus iure laudantium est non nesciunt expedita
+            voluptatem? Officia aspernatur veritatis fugiat quaerat, ab
+            laboriosam mollitia ipsum, ratione illum doloribus maxime molestias!
+            Obcaecati quis temporibus nobis nulla perferendis reprehenderit
+            exercitationem deserunt.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
+
+export default Landing;
