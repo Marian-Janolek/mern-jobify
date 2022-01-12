fdb6ed6b40ffe2f7b3d85d77108b1e39c93af5fe
Marian-Janolek <m.janolek@gmail.com>
2022-01-12 18:14:11 +0100

    add job model and create job

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,18 @@
-const createJob = (req, res) => {
-  res.send('create job');
+import Job from '../models/Job.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, UnAuthenticatedError } from '../errors/index.js';
+
+const createJob = async (req, res) => {
+  const { position, company } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError('Provide all values');
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
+
 const deleteJob = (req, res) => {
   res.send('delete job');
 };
