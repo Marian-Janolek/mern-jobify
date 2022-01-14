660671c7739c88664e8b5b4c9afe5b8eead4c914
Marian-Janolek <m.janolek@gmail.com>
2022-01-14 13:52:32 +0100

    add job container , edit job

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -15,6 +15,9 @@ import {
   CREATE_JOB_ERROR,
   CREATE_JOB_SUCCESS,
   CREATE_JOB_BEGIN,
+  GET_JOBS_BEGIN,
+  GET_JOBS_SUCCESS,
+  SET_EDIT_JOB,
 } from './action';
 import reducer from './reducer';
 import axios from 'axios';
@@ -41,6 +44,10 @@ const initialState = {
   statusOptions: ['pending', 'interview', 'declined'],
   status: 'pending',
   showSidebar: false,
+  jobs: [],
+  totalJobs: 0,
+  numOfPages: 1,
+  page: 1,
 };
 
 const AppContext = createContext();
@@ -184,6 +191,35 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
+  const getJobs = async () => {
+    let url = `/jobs`;
+    dispatch({ type: GET_JOBS_BEGIN });
+    try {
+      const { data } = await authFetch.get(url);
+      const { jobs, totalJobs, numOfPages } = data;
+      dispatch({
+        type: GET_JOBS_SUCCESS,
+        payload: { jobs, totalJobs, numOfPages },
+      });
+    } catch (error) {
+      console.log(error.reponse);
+      // logoutUser()
+    }
+    clearAlert();
+  };
+
+  const setEditJob = (id) => {
+    dispatch({ type: SET_EDIT_JOB, payload: { id } });
+  };
+
+  const editJob = () => {
+    console.log('edit job');
+  };
+
+  const deleteJob = (id) => {
+    console.log(`delete job : ${id}`);
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -196,6 +232,10 @@ const AppProvider = ({ children }) => {
         handleChange,
         clearValues,
         createJob,
+        getJobs,
+        setEditJob,
+        deleteJob,
+        editJob,
       }}
     >
       {children}
--- client/src/pages/dashboard/AllJob.jsx
+++ client/src/pages/dashboard/AllJob.jsx
@@ -1,5 +1,12 @@
+import { JobsContainer, SearchContainer } from '../../components';
+
 const AllJob = () => {
-  return <h1>all job page</h1>;
+  return (
+    <>
+      <SearchContainer />
+      <JobsContainer />
+    </>
+  );
 };
 
 export default AllJob;
